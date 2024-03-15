c27060afb24798b84d6d3db59944a5df980476dc
sabina <sabina.pauric@student.um.si>
2024-03-15 22:25:04 +0100

    feat: added css to components InfoPage.js and InfoPage2.js

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -11,19 +11,39 @@ const InfoPage = ({ locationData }) => {
     };
 
     return (
-        <div className="location-info">
-            <img src="/images/zrce-hr.jpg" alt="Zrce" className="location-image" />
-            <div className="location-details">
-                <h2> Zrce </h2>
-                <p>Lokacija: Hrvaška</p>
-                <div className="description">
-                    <p> Zrce je zakon. To je super lokacija za počitnice ob morju.</p>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <img src="/images/zrce-hr.jpg" alt="Zrce" className="img-fluid" />
                 </div>
-                <button onClick={handleButtonClick}>Več informacij</button>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div className="location-details">
+                        <h2 className="font-weight-bold display-1">Zrce</h2>
+
+                        <div style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginBottom: '1rem'
+                        }}>
+                            <img src="/images/location-outline.svg" alt="Opis slike"
+                                 style={{width: 'auto', height: '1em', marginRight: '0.5rem', maxHeight: '1em'}}/>
+                            <p style={{margin: 0}}>Lokacija: Hrvaška</p>
+                        </div>
+                        <div className="description bg-dark text-white p-3 rounded mb-3">
+                            <p>Zrce je zakon. To je super lokacija za počitnice ob morju.</p>
+                        </div>
+
+                        <button className="btn btn-outline-secondary" onClick={handleButtonClick}>
+                            Več informacij
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
-
 
 export default InfoPage;
