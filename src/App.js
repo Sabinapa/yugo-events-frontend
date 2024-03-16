1a03639969d120885468ac0c36f01506f635ff96
sabina <sabina.pauric@student.um.si>
2024-03-16 16:13:55 +0100

    fix: fixes routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MapView from "./components/MapView";
 import React from 'react';
-import Zdruzeno from './components/Zdruzeno';
 import InfoPage from "./components/InfoPage";
 import InfoPageMoreInfo from "./components/InfoPage2";
 import EventsPage from "./components/events";
@@ -15,12 +13,11 @@ function App() {
           <script src="https://cdn.jsdelivr.net/npm/openlayers@6.6.1/dist/ol.js"></script>
           <header className="Yugo-events">
             <Routes>
-                <Route path="/info" element={<InfoPage/>}></Route>
-                <Route path="/info-page" element={<InfoPageMoreInfo/>}></Route>
-                <Route path="/map" element={<MapView/>}></Route>
-                <Route path="/" element={<MapView />}></Route>
-                <Route path="/events" element={<EventsPage/>}></Route>
+                <Route path="/info" element={<InfoPage />}></Route>
                 <Route path="/info-page" element={<InfoPageMoreInfo />}></Route>
+                <Route path="/" element={<MapView />}></Route>
+                <Route path="/map" element={<MapView/>}></Route>
+                <Route path="/events" element={<EventsPage/>}></Route>
             </Routes>
         </header>
       </div>
